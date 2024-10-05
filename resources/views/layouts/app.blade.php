7c6e4419149b7fa481bc7ece462e70c6189609d8
jandycabilla <jandycabilla@gmail.com>
2024-10-05 08:47:07 +0800

    Added custom styles with style.css and linked it in the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,23 @@
     <title>Motorcycles App</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="{{ route('home') }}">MotorcyclesApp</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('motorcycles.index') }}">View Motorcycles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('motorcycles.create') }}">Add Motorcycle</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="container my-5">
         @yield('content')
     </div>
@@ -15,7 +30,5 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
 </body>
 </html>
